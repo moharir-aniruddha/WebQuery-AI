977bd78eaf154f47b6151d8419aaf0ac76decf99
moharir-aniruddha <m98.aniruddha@gmail.com>
2026-02-13 23:16:10 +0530

    Added PDF upload support with multi-file processing and improved source handling

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from rag import process_urls, generate_answer
+from rag import process_urls, process_pdf, generate_answer
 
-st.set_page_config(page_title="Real Estate RAG", layout="wide")
+
+st.set_page_config(page_title="WebQuery AI", layout="wide")
 
 st.title("WebQuery AI")
 
@@ -12,43 +13,100 @@ if "data_ready" not in st.session_state:
 if "processing" not in st.session_state:
     st.session_state.processing = False
 
+if "mode" not in st.session_state:
+    st.session_state.mode = "URL"
+
 
 # ---------------- SIDEBAR ---------------- #
-st.sidebar.header("🔗 Add Article URLs (Max 3)")
 
-url1 = st.sidebar.text_input("URL 1")
-url2 = st.sidebar.text_input("URL 2")
-url3 = st.sidebar.text_input("URL 3")
+st.sidebar.header("📂 Choose Input Type")
 
-urls = [u for u in [url1, url2, url3] if u]
+mode = st.sidebar.radio(
+    "Select Data Source",
+    ["URL", "PDF"]
+)
 
-if st.sidebar.button("⚙️ Process URLs"):
-    if len(urls) == 0:
-        st.sidebar.warning("Please enter at least one URL")
-    else:
-        st.session_state.processing = True
-        st.session_state.data_ready = False
+st.session_state.mode = mode
 
+
+# ---------------- URL MODE ---------------- #
+if mode == "URL":
+    st.sidebar.header("🔗 Add Article URLs (Max 3)")
+
+    url1 = st.sidebar.text_input("URL 1")
+    url2 = st.sidebar.text_input("URL 2")
+    url3 = st.sidebar.text_input("URL 3")
+
+    urls = [u for u in [url1, url2, url3] if u]
+
+    if st.sidebar.button("⚙️ Process URLs"):
+        if len(urls) == 0:
+            st.sidebar.warning("Please enter at least one URL")
+        else:
+            st.session_state.processing = True
+            st.session_state.data_ready = False
+            st.session_state.urls = urls
+
+
+# ---------------- PDF MODE ---------------- #
+if mode == "PDF":
+    st.sidebar.header("📄 Upload PDF Document")
+
+    uploaded_files = st.sidebar.file_uploader(
+        "Upload one or more PDF files",
+        type=["pdf"],
+        accept_multiple_files=True
+    )
+
+    if st.sidebar.button("⚙️ Process PDF"):
+        if not uploaded_files:
+            st.sidebar.warning("Please upload at least one PDF file")
+        else:
+            st.session_state.processing = True
+            st.session_state.data_ready = False
+            st.session_state.uploaded_files = uploaded_files
 
 # ---------------- MAIN SCREEN LOGIC ---------------- #
 
 # 🟡 STEP 1 — Show processing status
 if st.session_state.processing and not st.session_state.data_ready:
-    st.subheader("⏳ Processing Articles...")
 
+    st.subheader("⏳ Processing Knowledge Base...")
     status_box = st.empty()
 
-    for status in process_urls(urls):
-        status_box.info(status)
+    # URL Processing
+    if st.session_state.mode == "URL":
+        for status in process_urls(st.session_state.urls):
+            status_box.info(status)
 
-    status_box.success("✅ URLs parsed and knowledge base created!")
+    # PDF Processing
+    if st.session_state.mode == "PDF":
+
+        for index, file in enumerate(st.session_state.uploaded_files):
+            status_box.info(f"Processing {file.name}...")
+
+            # Reset only for first file
+            if index == 0:
+                result = process_pdf(file, reset=True)
+            else:
+                result = process_pdf(file, reset=False)
+
+            if isinstance(result, str):
+                status_box.error(result)
+                st.session_state.processing = False
+                break
+
+        status_box.info("Extracting and indexing all PDF content...")
+
+    status_box.success("✅ Knowledge base created successfully!")
     st.session_state.data_ready = True
     st.session_state.processing = False
 
 
 # 🟢 STEP 2 — Show Q&A interface AFTER processing
 if st.session_state.data_ready:
-    st.subheader("💬 Ask Questions from Articles")
+
+    st.subheader("💬 Ask Questions")
 
     query = st.text_input("Enter your question:")
 
--- rag.py
+++ rag.py
@@ -2,10 +2,11 @@ from uuid import uuid4
 from dotenv import load_dotenv
 from pathlib import Path
 
+from langchain_classic import text_splitter
 from langchain_classic.chains.qa_with_sources.retrieval import RetrievalQAWithSourcesChain
 from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain_community.document_loaders import WebBaseLoader
-
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
@@ -17,6 +18,11 @@ chunk_size = 500
 embedding_model = "sentence-transformers/all-MiniLM-L6-v2"
 vectorStore_dir = Path(__file__).parent/"resources/vectorstore"
 collection_name = "real_estate"
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", ".", " "],
+    chunk_size=500,
+    chunk_overlap=100
+)
 
 llm = None
 vector_store = None
@@ -46,11 +52,6 @@ def process_urls(url):
     data = loader.load()
 
     yield "Splitting Data"
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n", ".", " "],
-        chunk_size=500,
-        chunk_overlap=100
-    )
 
     docs = text_splitter.split_documents(data)
 
@@ -58,14 +59,65 @@ def process_urls(url):
     uuids = [str(uuid4()) for _ in range(len(docs))]
     vector_store.add_documents(documents=docs, ids=uuids)
 
+def process_pdf(uploaded_file, reset=False):
+    initialize_components()
+
+    try:
+        if reset:
+            vector_store.reset_collection()
+
+        temp_path = "temp_uploaded.pdf"
+        with open(temp_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+
+        loader = PyPDFLoader(temp_path)
+        data = loader.load()
+
+        for doc in data:
+            doc.metadata["source"] = uploaded_file.name
+
+        docs = text_splitter.split_documents(data)
+
+        uuids = [str(uuid4()) for _ in range(len(docs))]
+        vector_store.add_documents(documents=docs, ids=uuids)
+
+        vector_store.persist()
+
+    except Exception as e:
+        return str(e)
+
+
 def generate_answer(query):
     if not vector_store:
-        raise RuntimeError("No vector store")
+        raise RuntimeError("No vector store initialized")
 
-    chain = RetrievalQAWithSourcesChain.from_llm(llm=llm , retriever=vector_store.as_retriever())
-    result = chain.invoke({"question" : query} , return_only_outputs=True)
-    sources = result.get("sources" , "")
-    return result['answer'],sources
+    if "summary" in query.lower() or "summarize" in query.lower():
+        retriever = vector_store.as_retriever(search_kwargs={"k": 15})
+    else:
+        retriever = vector_store.as_retriever(search_kwargs={"k": 4})
+
+    chain = RetrievalQAWithSourcesChain.from_llm(
+        llm=llm,
+        retriever=retriever
+    )
+
+    result = chain.invoke(
+        {"question": query},
+        return_only_outputs=True
+    )
+
+    answer = result.get("answer", "")
+    raw_sources = result.get("sources", "")
+
+    # Clean and format sources
+    if raw_sources:
+        source_list = list(set([s.strip() for s in raw_sources.split(",")]))
+        formatted_sources = "\n".join(source_list)
+    else:
+        formatted_sources = "No sources found."
+
+    return answer, formatted_sources
+
 
 
 if __name__ == "__main__":
