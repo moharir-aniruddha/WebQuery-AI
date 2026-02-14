6be4efd9efc7be7cbd5d3c0ab460942e51db418c
moharir-aniruddha <m98.aniruddha@gmail.com>
2026-02-14 09:37:24 +0530

    tuned the number of relevent chunks

--- rag.py
+++ rag.py
@@ -94,7 +94,7 @@ def generate_answer(query):
     if "summary" in query.lower() or "summarize" in query.lower():
         retriever = vector_store.as_retriever(search_kwargs={"k": 15})
     else:
-        retriever = vector_store.as_retriever(search_kwargs={"k": 4})
+        retriever = vector_store.as_retriever(search_kwargs={"k": 6})
 
     chain = RetrievalQAWithSourcesChain.from_llm(
         llm=llm,
